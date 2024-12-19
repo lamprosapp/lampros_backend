2f6da69616a0b0422ea132d722a420d0ea1b8e4f
lamprosapp <applampros@gmail.com>
2024-12-20 04:39:32 +0530

    Update pro-post.js

--- routes/pro-post.js
+++ routes/pro-post.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addPost, listAllPosts, listUserPosts } from '../controllers/pro-post.js';
+import { addPost, deletePost, flagPost, listAllPosts, listUserPosts } from '../controllers/pro-post.js';
 import { protect } from '../middlewares/protect.js'; 
 
 // Initialize the router
@@ -7,6 +7,12 @@ const router = express.Router();
 
 // Route to add a new post (POST /api/posts)
 router.post('/posts', protect,  addPost);
+
+// Route to delete a post by its ID (DELETE /api/posts/:postId)
+router.delete('/posts/:postId', protect,  deletePost);
+
+// Route to flag/report a post  (POST /api/posts/:postId)
+router.post('/posts/:postId', protect,  flagPost);
 
 // Route to list all posts (GET /api/posts/all)
 router.get('/posts/all', listAllPosts);
