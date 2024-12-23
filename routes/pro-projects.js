14b55dbd35b2b1e898fb6d37464f03f55affa411
lamprosapp <applampros@gmail.com>
2024-12-23 10:40:16 +0530

    Update pro-projects.js

--- routes/pro-projects.js
+++ routes/pro-projects.js
@@ -1,11 +1,14 @@
 import express from 'express';
-import { 
-  addProject, 
-  listAllProjects, 
-  listUserProjects, 
-  filterProjects, 
-  generalSearchProjects, 
-  listAllProjectsByIds 
+import {
+  addProject,
+  listAllProjects,
+  listUserProjects,
+  filterProjects,
+  generalSearchProjects,
+  listAllProjectsByIds,
+  deleteProject,
+  flagProject,
+  clearFlags
 } from '../controllers/pro-projects.js';
 import { protect } from '../middlewares/protect.js';
 
@@ -26,6 +29,16 @@ router.post('/projects', protect, addProject);
 // Route to list all projects (GET /api/projects/all)
 router.get('/projects/all', conditionalProtect, listAllProjects);
 router.post('/projects/Ids', protect, listAllProjectsByIds);
+
+// Route to flag/report a project  (POST /api/projects/project/flag)
+router.post('/project/flag', protect,  flagProject);
+
+// Route to clear flag/report of a project  (POST /api/projects/project/clearFlag)
+router.post('/project/clearFlag', protect,  clearFlags);
+
+
+
+router.delete('/project/:projectId', protect, deleteProject);
 
 // Route to list projects created by the authenticated user (GET /api/projects/user)
 router.get('/projects/user', protect, listUserProjects);
