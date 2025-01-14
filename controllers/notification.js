10605f29c20df141d2509283bf4f26fd3beed5a2
lamprosapp <applampros@gmail.com>
2025-01-14 13:45:13 +0530

    Update notification.js

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,4 +1,4 @@
-import admin from 'firebase-admin';
+import admin from '../config/firebase';
 import dotenv from 'dotenv';
 import Notification from '../models/notification.js'; // Import your Mongoose model
 
