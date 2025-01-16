8dc40bd64ee4c9c2442192015029bba215579cdf
lamprosapp <applampros@gmail.com>
2025-01-17 01:29:37 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,7 @@
 import admin from 'firebase-admin';
 
 if (!admin.apps.length) {
-   admin.initializeApp({
+   const admi = admin.initializeApp({
         credential: admin.credential.cert({
           project_id: process.env.FIREBASE_PROJECT_ID,
           private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
