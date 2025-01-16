57f94d4f71a3560365b0366977740d4c66342131
lamprosapp <applampros@gmail.com>
2025-01-17 04:20:33 +0530

    Update notification.js

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,4 +1,4 @@
-import admin from 'firebase-admin';
+import admin from '../config/firebase-config.js';
 import dotenv from 'dotenv';
 import Notification from '../models/notification.js'; // Import your Mongoose model
 
@@ -31,7 +31,7 @@ const initializeFirebase = () => {
 };
 
 
-initializeFirebase();
+// initializeFirebase();
 
 // Send notification to a single device
 export const sendNotificationToDevice = async (token, title, body, userId) => {
