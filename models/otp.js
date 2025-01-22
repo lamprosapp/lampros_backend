3dd6a108950c4e554c7f80204d977fc5c30ae242
lamprosapp <applampros@gmail.com>
2025-01-22 22:03:56 +0530

    Update otp.js

--- models/otp.js
+++ models/otp.js
@@ -5,13 +5,13 @@ const otpSchema = new mongoose.Schema({
   phoneNumber: { type: String, required: true },
   otp: { type: String, required: true },
   expiresAt: { type: Date, required: true },
-  isVerified: { type: Boolean, default: false }, // Track OTP verification status
+  isVerified: { type: Boolean, default: true }, // Track OTP verification status
   createdAt: { type: Date, default: Date.now }, // Track when the OTP was created
 });
 
 otpSchema.methods.generateOtp = function() {
   // this.otp = crypto.randomInt(1000, 9999).toString();
-  this.otp = '5555'
+  // this.otp = '5555'
   this.expiresAt = Date.now() + 5 * 60 * 1000; // OTP expires in 5 minutes
 };
 
