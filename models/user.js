a4d44d55a3ad597873372317b80ac875c215ee01
HashirMusthafa <hashirvaliyil8146@gmail.com>
2025-02-21 10:38:11 +0530

    Feat: Added default profilepic based on gender

--- models/user.js
+++ models/user.js
@@ -46,7 +46,6 @@ const UserSchema = new mongoose.Schema(
     email: { type: String },
     isVerified: { type: Boolean, default: false }, // New isVerified field for email verification
     phoneNumber: { type: String, required: true, unique: true },
-    gender: { type: String },
     fname: { type: String }, // First name
     lname: { type: String }, // Last name
     age: { type: Number },
