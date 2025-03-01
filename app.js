888e30c94f8a1104914fbb56bed05b4a8c8970f9
HashirMusthafa <hashirvaliyil8146@gmail.com>
2025-03-01 13:15:04 +0530

    Feat: Added admin base protect routes

--- app.js
+++ app.js
@@ -1,38 +1,35 @@
-import express from 'express';
-import {logger} from './config/loggingConfig.js';
-import connectDB from './config/dbConfig.js';
-import corsMiddleware from './middlewares/corsMiddleware.js';
-import config from './config/serverConfig.js';
-import errorHandler from './middlewares/errorHandler.js';
-import { protect } from './middlewares/protect.js'; 
+import express from "express";
+import { logger } from "./config/loggingConfig.js";
+import connectDB from "./config/dbConfig.js";
+import corsMiddleware from "./middlewares/corsMiddleware.js";
+import config from "./config/serverConfig.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import { adminProtect, protect } from "./middlewares/protect.js";
 
-
-import ro_user from './routes/user.js'
-import ro_utils from './routes/utils.js'
-import ro_posts from './routes/pro-post.js'
-import ro_projects from './routes/pro-projects.js'
-import ro_products from './routes/pro-products.js'
-import category from './routes/catogory.js'
-import ro_brands from './routes/brand.js'
-import searchRoutes from './routes/serch.js'
-import ro_message from './routes/message.js'
-import ro_notification from './routes/notification.js'
-import ro_order from './routes/order.js'
-import ro_enq from './routes/enq.js'
-import ro_fo from './routes/followersRoutes.js'
-import ro_adr from './routes/deliveryAddressRoutes.js'
-import ro_review from './routes/review.js'
-import ro_qs from './routes/qs.js'
-import ro_ans from './routes/ans.js'
-import instantService from './routes/instantService.js'
-
+import ro_user from "./routes/user.js";
+import ro_utils from "./routes/utils.js";
+import ro_posts from "./routes/pro-post.js";
+import ro_projects from "./routes/pro-projects.js";
+import ro_products from "./routes/pro-products.js";
+import category from "./routes/catogory.js";
+import ro_brands from "./routes/brand.js";
+import searchRoutes from "./routes/serch.js";
+import ro_message from "./routes/message.js";
+import ro_notification from "./routes/notification.js";
+import ro_order from "./routes/order.js";
+import ro_enq from "./routes/enq.js";
+import ro_fo from "./routes/followersRoutes.js";
+import ro_adr from "./routes/deliveryAddressRoutes.js";
+import ro_review from "./routes/review.js";
+import ro_qs from "./routes/qs.js";
+import ro_ans from "./routes/ans.js";
+import instantService from "./routes/instantService.js";
+import adminBaseRoutes from "./routes/admin/baseRoutes.js";
+import adminAuthRoutes from "./routes/admin/authRoutes.js";
 
 const app = express();
 
-
 connectDB();
-
-
 
 // app.use(rateLimiter);
 // Body parser middleware
@@ -42,40 +39,40 @@ app.use(corsMiddleware);
 // app.use(requestLoggerMiddleware);
 
 // Use OTP routes
-app.use('/api/user', ro_user);
-app.use('/api/utils', ro_utils);
-app.use('/api/posts', ro_posts)
-app.use('/api/projects', ro_projects);
-app.use('/api/products', ro_products);
-app.use('/api/category', category);
-app.use('/api/brand', ro_brands);
-app.use('/api/qs', ro_qs);
-app.use('/api/ans', ro_ans);
-app.use('/api', searchRoutes);
-app.use('/api', protect, ro_message);
-app.use('/api', protect, ro_notification);
-app.use('/api/order', protect, ro_order);
-app.use('/api',protect, ro_enq);
-app.use('/api',protect, ro_fo);
-app.use('/api/adr',protect, ro_adr);
-app.use('/api/review',protect, ro_review);
-app.use('/api/instantService',protect, instantService);
-
-
+app.use("/api/user", ro_user);
+app.use("/api/utils", ro_utils);
+app.use("/api/posts", ro_posts);
+app.use("/api/projects", ro_projects);
+app.use("/api/products", ro_products);
+app.use("/api/category", category);
+app.use("/api/brand", ro_brands);
+app.use("/api/qs", ro_qs);
+app.use("/api/ans", ro_ans);
+app.use("/api", searchRoutes);
+app.use("/api/admin/auth", adminAuthRoutes);
+app.use("/api", protect, ro_message);
+app.use("/api", protect, ro_notification);
+app.use("/api/order", protect, ro_order);
+app.use("/api", protect, ro_enq);
+app.use("/api", protect, ro_fo);
+app.use("/api/adr", protect, ro_adr);
+app.use("/api/review", protect, ro_review);
+app.use("/api/instantService", protect, instantService);
+app.use("/api/admin", protect, adminProtect, adminBaseRoutes);
 
 // Example route
-app.get('/', (req, res) => {
-  res.json({"status":"running in main branch now testing..."})
+app.get("/", (req, res) => {
+  res.json({ status: "running in main branch now testing..." });
 });
 
 app.use((req, res, next) => {
   res.status(404).json({
-    status: 'error',
-    message: 'Not Found',
+    status: "error",
+    message: "Not Found",
     error: {
       code: 404,
-      description: 'The requested resource could not be found'
-    }
+      description: "The requested resource could not be found",
+    },
   });
 });
 
@@ -84,6 +81,10 @@ app.use(errorHandler);
 
 const PORT = config.port;
 
-app.listen(PORT, () => logger.info(`Server running in ${config.nodeEnv} mode on https://localhost:${PORT}`));
+app.listen(PORT, () =>
+  logger.info(
+    `Server running in ${config.nodeEnv} mode on https://localhost:${PORT}`
+  )
+);
 
 export default app;
--- middlewares/protect.js
+++ middlewares/protect.js
@@ -1,22 +1,47 @@
-import express from 'express';
-import { verifyToken } from '../config/jwt.js';
+import express from "express";
+import { verifyToken } from "../config/jwt.js";
+import User from "../models/user.js";
 
-export const protect =  async(req, res, next) => {
+export const protect = async (req, res, next) => {
   let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
     try {
-      token = req.headers.authorization.split(' ')[1];
+      token = req.headers.authorization.split(" ")[1];
       const decoded = await verifyToken(token);
       req.user = decoded.id; // Add the decoded user ID to the request object
       next();
     } catch (error) {
-      res.status(401).json({ message: 'Not authorized, token failed', error });
+      res.status(401).json({ message: "Not authorized, token failed", error });
     }
   }
 
   if (!token) {
-    res.status(401).json({ message: 'Not authorized, no token' });
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+export const adminProtect = async (req, res, next) => {
+  try {
+    if (!req.user) {
+      return errorResponse(res, 401, "Not authorized, no user found");
+    }
+
+    const user = await User.findById(req.user);
+    if (!user) {
+      return errorResponse(res, 401, "User not found");
+    }
+
+    if (user.role !== "Admin") {
+      return errorResponse(res, 403, "Access denied, Admins only");
+    }
+
+    next(); // User has access, proceed to the next middleware
+  } catch (error) {
+    return errorResponse(res, 500, "Internal server error", error.message);
   }
 };
 
