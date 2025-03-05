c176a0cf6de9c43889c91c81524c85a0102777fd
HashirMusthafa <hashirvaliyil8146@gmail.com>
2025-03-05 11:44:29 +0530

    Feat: Added generate employee controller and route

--- routes/admin/superAdminRoutes.js
+++ routes/admin/superAdminRoutes.js
@@ -1,1 +1,8 @@
+import express from "express";
+import { generateEmployee } from "../../controllers/admin/super-admin/employee.js";
  
+const superAdminRoutes = express.Router();
+
+superAdminRoutes.post("/add-employee", generateEmployee);
+
+export default superAdminRoutes;
